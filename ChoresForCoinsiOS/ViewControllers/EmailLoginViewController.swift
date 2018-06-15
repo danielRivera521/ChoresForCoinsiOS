ae7b54f2270e81f2574b358ec9b15f0b762d2ec7
Daniel Rivera <drivera1@fullsail.edu>
2018-06-15 12:48:17 -0400

    added emailviewcontrollerpage

---
+++ ChoresForCoinsiOS/ViewControllers/EmailLoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EmailLoginViewController.swift
+//  ChoresForCoinsiOS
+//
+//  Created by Daniel Rivera on 6/15/18.
+//  Copyright © 2018 Daniel Rivera, Andrew Harrington. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+
+
+class EmailLoginViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+    
+
+}
