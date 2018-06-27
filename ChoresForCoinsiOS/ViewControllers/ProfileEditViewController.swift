7b1609fef7149a1c72c3b5d0491a747347361719
Andrew Harrington <AGHarrington@fullsail.edu>
2018-06-27 02:06:16 -0700

    - Validates text in text fields - Validates correct email format - Removed Password edit text field and replaced with an "Update Password" button. When pressed, it sends an change password request email to the user - Saves data to database, but for some reason, updating the user's display name isn't working - Alerts user if there was an error while saving to database

--- ChoresForCoinsiOS/ViewControllers/ProfileEditViewController.swift
+++ ChoresForCoinsiOS/ViewControllers/ProfileEditViewController.swift
@@ -6,16 +6,13 @@ class ProfileEditViewController: UIViewController, FUIAuthDelegate {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var parentKeyLabelSmall: UILabel!
     @IBOutlet weak var parentKeyLabel: UILabel!
     @IBOutlet weak var parentKeyTextField: UITextField!
     
     var authUI: FUIAuth?
-    var ref: DatabaseReference?
     
-    var uid: String?
     var email: String?
     var parentKey: String?
     var username: String?
@@ -23,65 +20,33 @@ class ProfileEditViewController: UIViewController, FUIAuthDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // get a reference of the database from Firebase
-        ref = Database.database().reference()
 
         // get current user
         let user = Auth.auth().currentUser
         if let user = user {
-            uid = user.uid
+            let uid = user.uid
             email = user.email
-
+            
             // get user data from database
-            ref?.child("user").child(uid!).observe(.value, with: { (snapshot) in
+            Database.database().reference().child("user").child(uid).observeSingleEvent(of: .value) { (snapshot) in
                 if let val = snapshot.value as? [String:Any] {
                     if let usernameUnwrapped = val["user_name"] as? String {
                         self.username = usernameUnwrapped
                         self.usernameTextField.text = usernameUnwrapped
                     }
-
+                    
                     // get parent key
                     if let parentKeyUnwrapped = val["parent_id"] as? String {
                         self.parentKey = parentKeyUnwrapped
-
-                        // get isParent
-                        if let isParentUnwrapped = val["user_parent"] as? Bool {
-                            self.isParent = isParentUnwrapped
-
-                            // check if user is parent. Turn on/off text fields accordingly
-                            if isParentUnwrapped {
-                                // for parent
-                                self.parentKeyLabel.isHidden = false
-                             //   self.parentKeyTextField.isHidden = true
-
-                                // set parentkey in label
-                                if let parentKeyUnwrapped = self.parentKey {
-                                    self.parentKeyLabel.text = parentKeyUnwrapped
-                                }
-                            } else {
-                                // for child
-                                self.parentKeyLabel.isHidden = true
-                           //     self.parentKeyTextField.isHidden = false
-
-                                // set parentkey in text field
-//                                if let parentKeyUnwrapped = self.parentKey {
-//                                    self.parentKeyTextField.text = parentKeyUnwrapped
-//                                }
-                            }
-                        }
+                        self.parentKeyLabel.text = parentKeyUnwrapped
                     }
                 }
-            })
+            }
         }
-
+        
         if let emailUnwrapped = email {
             emailTextField.text = emailUnwrapped
         }
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        // removeallobservers
     }
     
     @IBAction func toGoBack(_ sender: UIButton) {
@@ -89,85 +54,104 @@ class ProfileEditViewController: UIViewController, FUIAuthDelegate {
     }
     
     @IBAction func save(_ sender: UIButton) {
-//        // gather text from text fields and save to current user object and database
-//        if Auth.auth().currentUser != nil {
-//
-//            // if username is nil, then user hasn't entered one yet, needs to be saved on db
-//            if username == nil {
-//                // make sure text field ins't empty or nil
-//                if usernameTextField.text != nil {
-//                    // check that the text field value is not empty or whitespaces
-//                    if !usernameTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty {
-//                        // save to db
-//                        ref?.child("user/\(uid!)/user_name").setValue(usernameTextField.text!)
-//                    }
-//                }
-//                // if the username text field doesn't match, then that means the user changed it
-//            } else {
-//                if usernameTextField.text != nil {
-//                    // check that the text field value is not empty or whitespaces
-//                    if !usernameTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty {
-//                        // check if values don't match
-//                        if username != usernameTextField.text! {
-//                            // save data to database
-//                            ref?.child("user/\(uid!)/user_name").setValue(usernameTextField.text!)
-//                        }
-//                    }
-//                }
-//            }
-//
-//            // if the email text field doesn't match, then that means the user changed it
-//            if email != nil && emailTextField.text != nil {
-//                // check that the text field value is not empty or whitespaces
-//                if !emailTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty {
-//                    // check if values don't match
-//                    if email != emailTextField.text! {
-//                        // update email on firebase
-//                        Auth.auth().currentUser?.updateEmail(to: emailTextField.text!, completion: { (error) in
-//                            // TODO: add alert
-//                            if let error = error {
-//                                print(error)
-//                            }
-//                        })
-//                    }
-//                }
-//            }
-//
-//            // PASSWORD UPDATE... may need to watch tutorial on how to properly update a password!!!!!!!!!!
-//            if let newPassword = passwordTextField.text {
-//                // check that the text field value is not empty or whitespaces
-//                if !newPassword.trimmingCharacters(in: .whitespaces).isEmpty {
-//                    Auth.auth().currentUser?.updatePassword(to: newPassword, completion: { (error) in
-//                        if let error = error {
-//                            print(error)
-//                        }
-//                    })
-//                }
-//            }
-//
-//            // if user is child, check for change in parent key text field. Update firebase and object
-//            if let isParent = isParent {
-//                if !isParent {
-//                    if parentKey == nil {
-//                        if parentKeyTextField.text != nil {
-//                            if !parentKeyTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty {
-//                                ref?.child("user/\(uid!)/parent_key").setValue(parentKeyTextField.text!)
-//                            }
-//                        }
-//                    } else {
-//                        if parentKeyTextField.text != nil {
-//                            if !parentKeyTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty {
-//                                if parentKey != parentKeyTextField.text! {
-//                                    ref?.child("user/\(uid!)/parent_key").setValue(parentKeyTextField.text!)
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        dismiss(animated: true, completion: nil)
+        let ref = Database.database().reference()
+        
+        // gather text from text fields and save to current user object and database
+        if Auth.auth().currentUser != nil {
+            // set uid with unwrapped optional (trying to avoid issues with firebase and optionals)
+            var uid = ""
+            if let actualID = Auth.auth().currentUser?.uid {
+                uid = actualID
+            }
+            
+            _ = saveUsername(ref: ref, uid: uid)
+            _ = saveEmail(ref: ref, uid: uid)
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func saveUsername(ref: DatabaseReference, uid: String) -> Bool {
+        // check that the text field value is not empty or whitespaces
+        guard let _ = usernameTextField.text, !usernameTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty else {
+            // alert user something went wrong
+            AlertController.showAlert(self, title: "Warning", message: "Please enter something for Username")
+            return false
+        }
+        
+        // check if values don't match
+        if username != usernameTextField.text! {
+            var displayNameSuccess = true
+            
+            // update display name as well
+            let user = Auth.auth().currentUser?.createProfileChangeRequest()
+            user?.displayName = username
+            user?.commitChanges(completion: { (error) in
+                if let error = error {
+                    displayNameSuccess = false
+                    print(error.localizedDescription)
+                    // alert user something went wrong
+                    AlertController.showAlert(self, title: "Error", message: "Username was not saved")
+                }
+            })
+            
+            if displayNameSuccess {
+                // save data to database
+                ref.child("user/\(uid)/user_name").setValue(usernameTextField.text!)
+                
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func saveEmail(ref: DatabaseReference, uid: String) -> Bool {
+        // check that the text field value is not empty or whitespaces
+        guard let _ = emailTextField.text, !emailTextField.text!.trimmingCharacters(in: .whitespaces).isEmpty else {
+            // alert user something went wrong
+            AlertController.showAlert(self, title: "Warning", message: "Please enter something for Email")
+            return false
+        }
+        
+        // make sure email is correct format
+        guard let _ = emailTextField.text, isValidEmail(email: emailTextField.text!) else {
+            AlertController.showAlert(self, title: "Warning", message: "The email address is in an incorrect format.")
+            return false
+        }
+        
+        // check if values don't match
+        if email != emailTextField.text! {
+            
+            // update email on firebase
+            Auth.auth().currentUser?.updateEmail(to: emailTextField.text!, completion: { (error) in
+                if let error = error {
+                    print(error)
+                    AlertController.showAlert(self, title: "Error", message: "There was an error saving your new email. Please try again")
+                    return
+                }
+            })
+        }
+        
+        return false
+    }
+    
+    func isValidEmail(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+    
+    @IBAction func updatePassword(_ sender: UIButton) {
+        Auth.auth().sendPasswordReset(withEmail: self.email!) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+                AlertController.showAlert(self, title: "Warning", message: "There was an error in resetting your password.")
+                return
+            }
+            
+            AlertController.showAlert(self, title: "Alert", message: "An email was sent to reset your password.")
+        }
     }
     
     @IBAction func logout(_ sender: UIButton) {
