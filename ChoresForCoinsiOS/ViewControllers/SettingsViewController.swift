8153d8d593a08b98de8c63c15d83bcff437bc635
Andrew Harrington <AGHarrington@fullsail.edu>
2018-07-25 22:35:10 -0700

    - created 2 container views for settings view - embedded a parent and child settings view. This is to remove empty space in the child view

--- ChoresForCoinsiOS/ViewControllers/SettingsViewController.swift
+++ ChoresForCoinsiOS/ViewControllers/SettingsViewController.swift
@@ -11,21 +11,15 @@ import Firebase
 
 class SettingsViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var coinValueTextField: UITextField!
-    @IBOutlet weak var bonusDaySwitch: UISwitch!
-    @IBOutlet weak var multiplierValueTextField: UITextField!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var coinAmtLabel: UILabel!
     @IBOutlet weak var childRedeemView: UIView!
     @IBOutlet weak var profileButton: UIButton!
     @IBOutlet weak var redDot: UIImageView!
     @IBOutlet weak var bgImage: UIImageView!
-    @IBOutlet weak var coinValView: UIView!
-    @IBOutlet weak var bonusDayView: UIView!
-    @IBOutlet weak var BonusMultView: UIView!
-    @IBOutlet weak var backgroundView: UIView!
-    @IBOutlet var settingsSuperView: UIView!
     @IBOutlet weak var redeemAlertImageView: UIImageView!
+    @IBOutlet weak var parentSettingsView: UIView!
+    @IBOutlet weak var childSettingsView: UIView!
     
     var isFirstLoad = true
     var coinValue = 0
@@ -49,12 +43,6 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
         // get animation ready
         animRedeemView = AnimationHelper.createRedeemAnim(vc: self)
         
-        coinValueTextField.delegate = self
-        multiplierValueTextField.delegate = self
-        
-        // bonus toggle off by default
-        bonusDaySwitch.isOn = false
-        
         childRedeemView.isHidden = true
         
         if (Auth.auth().currentUser?.displayName) != nil{
@@ -63,9 +51,6 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
         
         //gets the firebase generated id
         userID = (Auth.auth().currentUser?.uid)!
-        
-        // show the correct background image based on user selection
-        getBackground()
         
         //gets the custom parent id created in the registration
         getParentId()
@@ -80,8 +65,6 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
     override func viewWillAppear(_ animated: Bool) {
         isUserParent()
         getPhoto()
-        // get app settings from database and fill out text fields and toggle button accordingly
-        getAppSettings()
     }
     
     override func didReceiveMemoryWarning() {
@@ -91,57 +74,11 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
     
     // when view is going to disappear, check and save the setting values
     override func viewWillDisappear(_ animated: Bool) {
-        // use parent id as key for each app setting object. This way all users with that parent id will have the save app settings... backgrounds are an individual setting
-        if let pid = parentID {
-            // get database object for app settings
-            let ref = Database.database().reference().child("app_settings/\(pid)")
-            
-            var coinValue = 1
-            var multValue: Double = 1
-            
-            // unwrap coin value and convert to int
-            if !(coinValueTextField.text?.trimmingCharacters(in: .whitespaces).isEmpty)! {
-                if let coinValUnwrapped = coinValueTextField.text {
-                    if let coinvalint = Int(coinValUnwrapped) {
-                        coinValue = coinvalint
-                    }
-                }
-            }
-            
-            // unwrap bonus value and convert to int
-            if !(multiplierValueTextField.text?.trimmingCharacters(in: .whitespaces).isEmpty)! {
-                if let multValueUnwrapped = multiplierValueTextField.text {
-                    if let multvalint = Double(multValueUnwrapped) {
-                        multValue = multvalint
-                    }
-                }
-            }
-            
-            // save coin value
-            ref.child("coin_dollar_value").setValue(coinValue)
-            // save bonus day toggle
-            ref.child("bonus_toggled").setValue(bonusDaySwitch.isOn)
-            //bonus coin value
-            ref.child("multiplier_value").setValue(multValue)
-            
-            print("Message from Settings View: mult = \(multValue)")
-        }
+        
     }
     
     
     // MARK: Custom Functions
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        self.view.endEditing(true)
-        
-        return true
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // dismiss keyboard when user touches outside of keyboard
-        self.view.endEditing(true)
-    }
     
     func displayHeaderName(){
         let databaseRef = Database.database().reference()
@@ -166,9 +103,6 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
                 let id = value?["parent_id"] as? String
                 if let actualID = id{
                     self.parentID = actualID
-                    
-                    // get app settings from database and fill out text fields and toggle button accordingly
-                    self.getAppSettings()
                 }
             }
         }
@@ -273,13 +207,13 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
                 let value = snapshot.value as? NSDictionary
                 //gets the image URL from the user database
                 if let profileURL = value?["profile_image_url"] as? String{
-                
+                    
                     let url = URL(string: profileURL)
                     ImageService.getImage(withURL: url!, completion: { (image) in
                         
                         self.profileButton.setBackgroundImage(image, for: .normal)
                     })
-              //      self.profileButton.loadImagesUsingCacheWithUrlString(urlString: profileURL, inViewController: self)
+                    //      self.profileButton.loadImagesUsingCacheWithUrlString(urlString: profileURL, inViewController: self)
                     //turn button into a circle
                     self.profileButton.layer.cornerRadius = self.profileButton.frame.width/2
                     self.profileButton.layer.masksToBounds = true
@@ -338,61 +272,16 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    func getBackground() {
-        if let uid = self.userID {
-            
-            Database.database().reference().child("user/\(uid)/bg_image").observeSingleEvent(of: .value) { (snapshot) in
-                if let value = snapshot.value as? Int {
-                    switch value {
-                    case 0:
-                        self.bgImage.image = #imageLiteral(resourceName: "whiteBG")
-                    case 1:
-                        self.bgImage.image = #imageLiteral(resourceName: "orangeBG")
-                    case 2:
-                        self.bgImage.image = #imageLiteral(resourceName: "greenBG")
-                    case 3:
-                        self.bgImage.image = #imageLiteral(resourceName: "redBG")
-                    case 4:
-                        self.bgImage.image = #imageLiteral(resourceName: "purpleBG")
-                    default:
-                        self.bgImage.image = #imageLiteral(resourceName: "whiteBG")
-                    }
-                }
-            }
-        }
-    }
-    
-    func getAppSettings() {
-        if let pid = parentID {
-            // get database object for app settings
-            let ref = Database.database().reference().child("app_settings/\(pid)")
-            
-            ref.observeSingleEvent(of: .value) { (snapshot) in
-                if let appSettings = snapshot.value as? NSDictionary {
-                    if let coinval = appSettings["coin_dollar_value"] as? Int {
-                        self.coinValueTextField.text = "\(coinval)"
-                    }
-                    
-                    if let bonusToggle = appSettings["bonus_toggled"] as? Bool {
-                        self.bonusDaySwitch.isOn = bonusToggle
-                    }
-                    
-                    if let multVal = appSettings["multiplier_value"] as? Double {
-                        self.multiplierValueTextField.text = "\(multVal)"
-                    }
-                }
-            }
-        }
-    }
-    
     func checkIfParent() {
         // checks if user is parent. If yes, go to parent coin view, else show redeem view
         Database.database().reference().child("user/\(userID!)/user_parent").observeSingleEvent(of: .value, with: { (snapshot) in
             if let isParent = snapshot.value as? Bool {
                 if !isParent {
-                    self.coinValView.isHidden = true
-                    self.bonusDayView.isHidden = true
-                    self.BonusMultView.isHidden = true
+                    self.childSettingsView.isHidden = false
+                    self.parentSettingsView.isHidden = true
+                } else {
+                    self.childSettingsView.isHidden = true
+                    self.parentSettingsView.isHidden = false
                 }
             }
         })
@@ -401,38 +290,7 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: Actions
     
-    @IBAction func toggleBonusDay(_ sender: UISwitch) {
-    }
     
-    @IBAction func selectBackground(_ sender: UIButton) {
-        if let uid = userID {
-            var bgSelection = 0
-            
-            // switch to determine which button was selected via tag
-            switch sender.tag {
-            case 1:
-                bgSelection = 0
-                self.bgImage.image = #imageLiteral(resourceName: "whiteBG")
-            case 2:
-                bgSelection = 1
-                self.bgImage.image = #imageLiteral(resourceName: "orangeBG")
-            case 3:
-                bgSelection = 2
-                self.bgImage.image = #imageLiteral(resourceName: "greenBG")
-            case 4:
-                bgSelection = 3
-                self.bgImage.image = #imageLiteral(resourceName: "redBG")
-            case 5:
-                bgSelection = 4
-                self.bgImage.image = #imageLiteral(resourceName: "purpleBG")
-            default:
-                bgSelection = 0
-                self.bgImage.image = #imageLiteral(resourceName: "whiteBG")
-            }
-            
-            Database.database().reference().child("user/\(uid)/bg_image").setValue(bgSelection)
-        }
-    }
     
     @IBAction func toCoinView(_ sender: UIButton) {
         // checks if user is parent. If yes, go to parent coin view, else show redeem view
@@ -480,7 +338,7 @@ class SettingsViewController: UIViewController, UITextFieldDelegate {
             present(alert, animated: true, completion: nil)
             
         }
-
+        
     }
     
 }
